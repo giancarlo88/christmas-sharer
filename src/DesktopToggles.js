810048c4e25405e1592f05ba90a7b50a4c0b66cf
Giancarlo Galliani <galliani.08@gmail.com>
2016-11-24 10:54:58 +0000

    fix transitions, add background-text, styling changes

--- src/DesktopToggles.js
+++ src/DesktopToggles.js
@@ -12,9 +12,11 @@ class DesktopToggles extends Component {
 
  render (){
    return(<div className="toggle-container" key={1}>
-            <div onClick={this.handleChoiceClick.bind(this,"gift-responses")} className="choice-toggle gift-responses"><h2>Gift Responses</h2></div>
-            <div onClick={this.handleChoiceClick.bind(this, "invitation-excuses")} className="choice-toggle invitation-excuses"><h2>Invitation Excuses</h2></div>
-            <div onClick={this.handleChoiceClick.bind(this, "seasons-greetings")} className="choice-toggle seasons-greetings"><h2>Seasons Greetings</h2></div>
+            <div className="toggle-button-container">
+              <div onClick={this.handleChoiceClick.bind(this,"gift-responses")} className="choice-toggle gift-responses"><h2>Gift Responses</h2></div>
+              <div onClick={this.handleChoiceClick.bind(this, "invitation-excuses")} className="choice-toggle invitation-excuses"><h2>Invitation Excuses</h2></div>
+              <div onClick={this.handleChoiceClick.bind(this, "seasons-greetings")} className="choice-toggle seasons-greetings"><h2>Seasons Greetings</h2></div>
+            </div>
           </div>
    )
 
